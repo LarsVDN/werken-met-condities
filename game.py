8386a7cdfcdcfaa1e0ccc6a74dbb4070afeff27e
Lars van den Nieuwendijk <larsvdnieuwendijk@t-mobilethuis.nl>
2021-10-08 14:13:04 +0200

    memes

--- game.py
+++ game.py
@@ -1,3 +1,15 @@
 print("--------------------------------------------------")
-print("               Dit is unnamed game                ")
+print("                 Unnamed game                     ")
 print("--------------------------------------------------")
+
+eersteVraag = input("Spelersnaam: ")
+if condition:
+    meme1 = input("Wat is 9+11?: ")
+    if condition:
+        if condition:
+            if condition:
+
+            else
+        else
+    else
+else

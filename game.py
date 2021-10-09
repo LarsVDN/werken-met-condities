b9eaa101bb9b560982b540bf7e38e4c2f4887629
Lars van den Nieuwendijk <larsvdnieuwendijk@t-mobilethuis.nl>
2021-10-09 10:35:13 +0200

    Taal menu + spelernaam

--- game.py
+++ game.py
@@ -2,14 +2,11 @@ print("--------------------------------------------------")
 print("                 Unnamed game                     ")
 print("--------------------------------------------------")
 
-eersteVraag = input("Spelersnaam: ")
-if condition:
-    meme1 = input("Wat is 9+11?: ")
-    if condition:
-        if condition:
-            if condition:
-
-            else
-        else
-    else
-else
+taal = input("Welke taal? / Which Language? (NL/EN): ")
+if taal == 'nl' or taal == 'NL':
+    spelerNaam = input("Welkom bij de game, wat is uw spelersnaam?: ")
+    print("Welkom bij de game " + spelerNaam)
+elif taal == 'en' or taal == 'EN':
+    print("Work in progress")
+else:
+    print("Deze taal is niet in het spel. / This language is not available in the game")
